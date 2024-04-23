b86f526422478623bd69abe2736e13c66b79e2ca
Naiara Andrade <andradenaiara@hotmail.com>
2024-04-23 01:05:46 -0300

    new: fix and add aws folder to credentials

--- projeto_aws/backend/app.py
+++ projeto_aws/backend/app.py
@@ -10,15 +10,13 @@ app = Flask(__name__)
 # loading variables from .env file
 load_dotenv() 
 
-AWS_KEY = os.getenv("AWS_KEY")
-AWS_SECRET = os.getenv("AWS_SECRET")
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
     if file:
-        #session = boto3.Session(profile_name="ada_s3")
-        s3_client = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='')
+        session = boto3.Session(profile_name="default")
+        s3_client = session.client('s3')
         s3_client.upload_fileobj(file, os.getenv("NAME_BUCKET"), file.filename)
         return 'File uploaded successfully to S3!'
     else:
