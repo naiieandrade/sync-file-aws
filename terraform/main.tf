6dbbf078acfdd7a8b0dc6a9852150361637315a9
Naiara Andrade <andradenaiara@hotmail.com>
2024-04-25 13:13:56 -0300

    ttf: add server side encryption in bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,11 +14,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# resource "aws_s3_bucket" "myawsbucket" {
+#   bucket = "myawsbucket-s3-ada"
+
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = "myawsbucket-s3-ada"
+
   versioning_configuration {
     status = "Enabled"
+  }
+  
+
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "aws:kms"
+  #     }
+  #   }
+  # }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = "myawsbucket-s3-ada"#.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      # kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "AES256"
+    }
   }
 }
 
