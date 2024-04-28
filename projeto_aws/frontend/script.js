c0c270d8ac974a70b75c2ee366c0f6d69d7c645c
Mikhaelle <mikhabueno@outlook.com>
2024-04-28 12:47:21 -0300

    listando arquivos no bucket e permitindo download

--- projeto_aws/frontend/script.js
+++ projeto_aws/frontend/script.js
@@ -1,23 +1,23 @@
 function uploadFile() {
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
-  
-      fetch('http://localhost:5000/upload', {
-        method: 'POST',
-        body: formData
-      })
-      .then(response => {
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+  if (file) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
         console.log(response);
-        alert('File uploaded successfully!');
+        alert("File uploaded successfully!");
       })
-      .catch(error => {
-        console.error('Error uploading file:', error);
-        alert('Error uploading file. Check the console for more details.');
+      .catch((error) => {
+        console.error("Error uploading file:", error);
+        alert("Error uploading file. Check the console for more details.");
       });
-    } else {
-      alert('Select a file to upload.');
-    }
+  } else {
+    alert("Select a file to upload.");
   }
+}
